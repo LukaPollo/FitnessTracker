aed8c620f7adc660b89a594d9921fb624e6142e8
Podiashvili Luka 956 <podluk956@logiker.hu>
2026-02-20 09:39:31 +0100

    Added textboxes and buttons to Form1.Desginer

--- FitnessTracker/Form1.Designer.cs
+++ FitnessTracker/Form1.Designer.cs
@@ -28,21 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.SubmitBtn = new System.Windows.Forms.Button();
+            this.textBoxSport = new System.Windows.Forms.TextBox();
+            this.textBoxTime = new System.Windows.Forms.TextBox();
+            this.textBoxDate = new System.Windows.Forms.TextBox();
+            this.textBoxLocation = new System.Windows.Forms.TextBox();
+            this.ExportBtn = new System.Windows.Forms.Button();
+            this.ImportBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // SubmitBtn
+            // 
+            this.SubmitBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SubmitBtn.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SubmitBtn.Location = new System.Drawing.Point(12, 277);
+            this.SubmitBtn.Name = "SubmitBtn";
+            this.SubmitBtn.Size = new System.Drawing.Size(100, 29);
+            this.SubmitBtn.TabIndex = 0;
+            this.SubmitBtn.Text = "Submit";
+            this.SubmitBtn.UseVisualStyleBackColor = true;
+            this.SubmitBtn.Click += new System.EventHandler(this.SubmitBtn_Click);
+            // 
+            // textBoxSport
+            // 
+            this.textBoxSport.Location = new System.Drawing.Point(12, 27);
+            this.textBoxSport.Name = "textBoxSport";
+            this.textBoxSport.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSport.TabIndex = 2;
+            this.textBoxSport.TextChanged += new System.EventHandler(this.textBoxSport_TextChanged);
+            // 
+            // textBoxTime
+            // 
+            this.textBoxTime.Location = new System.Drawing.Point(12, 146);
+            this.textBoxTime.Name = "textBoxTime";
+            this.textBoxTime.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTime.TabIndex = 3;
+            this.textBoxTime.TextChanged += new System.EventHandler(this.textBoxTime_TextChanged);
+            // 
+            // textBoxDate
+            // 
+            this.textBoxDate.Location = new System.Drawing.Point(12, 85);
+            this.textBoxDate.Name = "textBoxDate";
+            this.textBoxDate.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDate.TabIndex = 4;
+            this.textBoxDate.TextChanged += new System.EventHandler(this.textBoxDate_TextChanged);
+            // 
+            // textBoxLocation
+            // 
+            this.textBoxLocation.Location = new System.Drawing.Point(12, 209);
+            this.textBoxLocation.Name = "textBoxLocation";
+            this.textBoxLocation.Size = new System.Drawing.Size(100, 20);
+            this.textBoxLocation.TabIndex = 5;
+            this.textBoxLocation.TextChanged += new System.EventHandler(this.textBoxLocation_TextChanged);
+            // 
+            // ExportBtn
+            // 
+            this.ExportBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ExportBtn.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExportBtn.Location = new System.Drawing.Point(12, 341);
+            this.ExportBtn.Name = "ExportBtn";
+            this.ExportBtn.Size = new System.Drawing.Size(100, 29);
+            this.ExportBtn.TabIndex = 6;
+            this.ExportBtn.Text = "Export";
+            this.ExportBtn.UseVisualStyleBackColor = true;
+            // 
+            // ImportBtn
+            // 
+            this.ImportBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ImportBtn.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ImportBtn.Location = new System.Drawing.Point(12, 409);
+            this.ImportBtn.Name = "ImportBtn";
+            this.ImportBtn.Size = new System.Drawing.Size(100, 29);
+            this.ImportBtn.TabIndex = 7;
+            this.ImportBtn.Text = "Import";
+            this.ImportBtn.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(37, 1);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(50, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "Sport ág ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(37, 59);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(50, 20);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.Text = "Date";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(37, 120);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(50, 20);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.Text = "Time";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(37, 183);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(50, 20);
+            this.textBox4.TabIndex = 11;
+            this.textBox4.Text = "Location";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ImportBtn);
+            this.Controls.Add(this.ExportBtn);
+            this.Controls.Add(this.textBoxLocation);
+            this.Controls.Add(this.textBoxDate);
+            this.Controls.Add(this.textBoxTime);
+            this.Controls.Add(this.textBoxSport);
+            this.Controls.Add(this.SubmitBtn);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = " ";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button SubmitBtn;
+        private System.Windows.Forms.TextBox textBoxSport;
+        private System.Windows.Forms.TextBox textBoxTime;
+        private System.Windows.Forms.TextBox textBoxDate;
+        private System.Windows.Forms.TextBox textBoxLocation;
+        private System.Windows.Forms.Button ExportBtn;
+        private System.Windows.Forms.Button ImportBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
 
--- FitnessTracker/Form1.cs
+++ FitnessTracker/Form1.cs
@@ -12,12 +12,38 @@ namespace FitnessTracker
 {
     public partial class Form1 : Form
     {
+        public string textSportAg = "";
         public Form1()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxSport_TextChanged(object sender, EventArgs e)
+        {
+            textSportAg = textBoxSport.text;
+        }
+
+        private void textBoxDate_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxTime_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxLocation_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SubmitBtn_Click(object sender, EventArgs e)
         {
 
         }
